48ef953bcc203cdfaf7c4b53e057c8c877f25277
Matthias Kern <kern_matthias@gmx.at>
2022-10-23 14:24:49 +0200

    add functions for setting up external displays

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -3,3 +3,11 @@ function set_ldd_env() {
     export CROSS_COMPILE=arm-linux-gnueabihf-
 }
 
+function setup_displays() {
+    xrandr --output DP-1-2.2 --auto --left-of eDP-1 --output DP-1-2.1.6 --auto --left-of DP-1-2.2
+}
+
+function setup_keyboard() {
+    setxkbmap -layout de
+}
+
