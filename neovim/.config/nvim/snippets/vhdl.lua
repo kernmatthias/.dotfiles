f0ddb1facd792e87f9b609d9a6452903ae127ced
Matthias Kern <kern_matthias@gmx.at>
2026-05-15 12:21:38 +0200

    feat(nvim): update vhdl snippets

--- neovim/.config/nvim/snippets/vhdl.lua
+++ neovim/.config/nvim/snippets/vhdl.lua
@@ -10,6 +10,9 @@ local rep = require("luasnip.extras").rep
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("vhdl", {
-	s("stdul", { t("std_ulogic") }),
-	s("stdulv", { t("std_ulogic_vector") }),
+	s("sul", { t("std_ulogic") }),
+	s("sulv", { t("std_ulogic_vector") }),
+	s("i2sulv", { t("std_ulogic_vector(to_unsigned("), i(1, "num"), t(", "), i(2, "len"), t("))") }),
+	s("sulv2i", { t("to_integer(unsigned("), i(1, "sulv"), t("))") }),
+	s("hexi", { t("16#"), i(1, "0"), t("#") }),
 })
